a4f0f7cc051eb829e2107dc260bb89dcf63a121e
Ajay kumar Arya <ajaykumararya963983@gmail.com>
2025-03-25 14:58:26 +0530

    udpate demo

--- engine/models/Donor_model.php
+++ engine/models/Donor_model.php
@@ -9,7 +9,7 @@ class Donor_model extends MY_Model
             COALESCE(SUM(dr.amount),0) as total_donate')
             ->from('donor as d')
             ->join('state', 'state.STATE_ID = d.state_id')
-            ->join('donor_receipts as dr' ,'dr.donor_id = d.id','left')
+            ->join('donor_receipts as dr', 'dr.donor_id = d.id', 'left')
             ->group_by('d.id')
             ->join('district', 'district.DISTRICT_ID = d.city_id and district.STATE_ID = state.STATE_ID')
         ;
@@ -49,6 +49,30 @@ class Donor_model extends MY_Model
     function get_via_id($id)
     {
         return $this->get_switch('via_id', ['id' => $id]);
+    }
+    function list_front_donors()
+    {
+        $this->db->select('
+                d.name,
+                d.image,
+                d.mobile, 
+                district.DISTRICT_NAME as city, 
+                dr.amount AS last_donate, 
+                dr.receipt_date,
+                dr.mode
+            ')
+            ->from('donor AS d')
+            ->join('state', 'state.STATE_ID = d.state_id')
+            ->join('district', 'district.DISTRICT_ID = d.city_id AND district.STATE_ID = state.STATE_ID')
+            ->join('(SELECT donor_id, amount, date as receipt_date ,mode
+                    FROM table_donor_receipts 
+                    WHERE status = 1 
+                    AND id = (SELECT MAX(id) FROM table_donor_receipts AS sub WHERE sub.donor_id = table_donor_receipts.donor_id AND sub.status = 1)
+                    ) AS dr', 'dr.donor_id = d.id', 'inner')
+            ->where('d.status', 1);
+
+        return $this->db->get();
+
     }
 }
 ?>
