57125bc530b69286c868efc65e6a2acdbbed92aa
Ajay kumar Arya <ajaykumararya963983@gmail.com>
2025-04-14 13:30:43 +0530

    update common-header

--- modules/site/views/common-header.php
+++ modules/site/views/common-header.php
@@ -15,8 +15,8 @@
 <link href='https://fonts.googleapis.com/css?family=Reem Kufi' rel='stylesheet'>
 <script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.min.js"></script>
 
-    <script src="{base_url}assets/plugins/global/plugins.bundle.js"></script>
-    <script src="{base_url}assets/plugins/custom/datatables/datatables.bundle.js"></script>
+<script src="{base_url}assets/plugins/global/plugins.bundle.js"></script>
+<script src="{base_url}assets/plugins/custom/datatables/datatables.bundle.js"></script>
 <style>
     body,
     li,
@@ -38,8 +38,8 @@
         #printableContent {
             width: 98%;
             position: absolute;
-            left:2%;
-            top:2%
+            left: 2%;
+            top: 2%
         }
     }
 </style>
@@ -69,6 +69,122 @@
 
     .select2 {
         width: 100% !important;
+    }
+
+    .select2-container .select2-selection--single {
+        height: 36px !important
+    }
+
+    .image_div_col label i {
+        color: gray;
+    }
+
+    input[type=text]:focus,
+    input[type=password]:focus,
+    input[type=email]:focus,
+    input[type=file]:focus,
+    input[type=date]:focus,
+    option:focus,
+    input[type=number]:focus,
+    textarea:focus,
+    select:focus,
+    .select2-container--below.select2-container--open,
+    .select2-dropdown.select2-dropdown--below {
+        border: 1px solid #DF2800 !important;
+        box-shadow: 0px 0px 8px rgb(226, 176, 0) !important;
+    }
+
+    input[type=text],
+    input[type=password],
+    input[type=file],
+    input[type=email],
+    input[type=date],
+    input[type=number],
+    select,
+    textarea {
+        border: 1px solid rgb(226, 176, 0) !important;
+    }
+
+    .line-all {
+        width: 101%;
+        height: 2px;
+        margin-bottom: 20px;
+        margin-top: 8px;
+        background: rgb(255, 93, 0);
+        background: radial-gradient(circle, rgba(255, 93, 0, 1) 0%, rgba(255, 51, 0, 1) 100%);
+    }
+
+    #red {
+        color: red
+    }
+
+    .image_div_col label {
+        position: absolute;
+        font-size: 40px;
+        top: 50%;
+        transform: translateY(-50%);
+        cursor: pointer;
+        right: 30px;
+    }
+
+    .image_div_col img {
+        position: absolute;
+        height: 90%;
+        /* width: 100%; */
+        top: 50%;
+        transform: translateY(-50%);
+        left: 30px;
+    }
+
+    .image_div_col {
+        position: relative;
+        /* border: 1px solid red; */
+        border-radius: 10px;
+    }
+
+    .btn-grad {
+        background: rgb(255, 93, 0);
+        background: radial-gradient(circle, rgba(255, 93, 0, 1) 0%, rgba(255, 51, 0, 1) 100%);
+    }
+
+    .image_preview_div {
+        border: 2px solid gray;
+        height: 100% !important;
+        border-radius: 10px;
+        padding: 5px;
+    }
+
+    label {
+        font-weight: bold;
+        font-size: 14px;
+        color: rgb(51, 51, 51) !important;
+    }
+
+    .btn-grad {
+        text-align: center;
+        text-transform: uppercase;
+        transition: 0.5s;
+        background-size: 200% auto;
+        color: white;
+        box-shadow: 0 0 20px #eee;
+        border-radius: 10px;
+    }
+
+    .bname span {
+        font-weight: bold;
+        font-size: 12px;
+        margin-left: 8px;
+    }
+
+    .bg-theme,
+    .nav-bar,
+    .card1,
+    .right-div-p,
+    .left-div-p,
+    .buttons button,
+    .contact-footer {
+        background: rgb(255, 93, 0);
+        background: radial-gradient(circle, rgba(255, 93, 0, 1) 0%, rgba(255, 51, 0, 1) 100%);
     }
 </style>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
